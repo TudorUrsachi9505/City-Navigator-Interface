cbaa3658d9e0e301dad557e88f8e5c04e746adef
TudorUrsachi9505 <TudorUrsachi9505@users.noreply.github.com>
2018-05-20 11:19:56 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $('.materialboxed').materialbox();
+});
+
+$(document).ready(function(){
+      $('.carousel.carousel-slider').carousel({
+          fullWidth: true,
+          duration: 200,
+          indicators: true
+      });
+      
+        $(document).ready(function(){
+          $('.slider').slider();
+        });
+    });
