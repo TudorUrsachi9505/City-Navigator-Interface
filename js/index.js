e6a96c292076d4b22cebf2e921ff51f207d686ff
TudorUrsachi9505 <TudorUrsachi9505@users.noreply.github.com>
2018-05-20 15:03:12 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,16 @@
+// MaterialBox images
 $(document).ready(function(){
   $('.materialboxed').materialbox(); });
+// Carousel nearby locations
 $(document).ready(function(){
       $('.carousel.carousel-slider').carousel({
           fullWidth: true,
           duration: 200 });
         $(document).ready(function(){
           $('.slider').slider(); }); });
+// Collapse features
 $(document).ready(function(){
   $('.collapsible').collapsible(); });
+// Sidebar navigation page
+$(document).ready(function(){
+    $('.sidenav').sidenav(); });
